22e05cd425e3a3a1011ab599cc4cdcce223d1412
jusuri27 <jusuri27@gmail.com>
2024-10-21 19:05:49 +0900

    카테고리 관리 미리보기(제목, 설명, 이미지, 테마)  화면 추가

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -1,25 +1,68 @@
 <template>
   <div class="q-pa-md">
-    <q-table
-      flat bordered
-      title="카테고리 관리"
-      :rows="rows"
-      :columns="columns.filter(col => col.visible !== false)"
-      row-key="id"
-      :selected-rows-label="getSelectedString"
-      selection="single"
-      v-model:selected="selected"
-      @row-dblclick="onDbRowClick"
-      :pagination="initialPagination"
-    >
-      <template v-slot:top-right>
-        <div class="q-pa-md q-gutter-sm">
-          <q-btn color="secondary" @click="clickExcelDownload()" label="엑셀" />
-          <q-btn color="primary" @click="clickCreateCategory()" label="추가" />
-          <q-btn color="red" @click="clickDeleteCategory()" label="삭제" />
-        </div>
-      </template>
-    </q-table>
+    <div class="q-col-gutter-md row items-start">
+      <div class="col-3">
+        <q-card class="my-card" flat bordered>
+          <q-card-section>
+            <q-img 
+            style="min-width: 200px; max-width: 540px; max-height: 360px;"
+            :style="{ backgroundColor: selectedRow?.themeClass || '#f4f4f4' }"
+            :src="selectedRow?.imageUrl || 'https://cdn.quasar.dev/img/parallax2.jpg'"
+             />
+            <div class="text-overline text-orange-9">Overline</div>
+            <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+            <div class="text-caption text-grey">
+              <q-item-label caption lines="2">{{ selectedRow?.title || 'Secondary line text.' }}</q-item-label>
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+            <q-space />
+
+            <q-btn
+              color="grey"
+              round
+              flat
+              dense
+              :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+              @click="expanded = !expanded"
+            />
+          </q-card-actions>
+
+          <q-slide-transition>
+            <div v-show="expanded">
+              <q-separator />
+              <q-card-section class="text-subtitle2">
+                <q-item-label caption lines="2">{{ selectedRow?.description || 'Secondary line text.' }}</q-item-label>
+              </q-card-section>
+            </div>
+          </q-slide-transition>
+        </q-card>
+      </div>
+      <div class="col-9">
+        <q-table
+          flat bordered
+          title="카테고리 관리"
+          :rows="rows"
+          :columns="columns.filter(col => col.visible !== false)"
+          row-key="id"
+          :selected-rows-label="getSelectedString"
+          selection="single"
+          v-model:selected="selected"
+          @row-click="onRowClick"
+          @row-dblclick="onDbRowClick"
+          :pagination="initialPagination"
+        >
+          <template v-slot:top-right>
+            <div class="q-pa-md q-gutter-sm">
+              <q-btn color="secondary" @click="clickExcelDownload()" label="엑셀" />
+              <q-btn color="primary" @click="clickCreateCategory()" label="추가" />
+              <q-btn color="red" @click="clickDeleteCategory()" label="삭제" />
+            </div>
+          </template>
+        </q-table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -50,10 +93,18 @@ const columns = [
   { name: 'themeClass', align: 'left', label: '테마', field: 'themeClass', sortable: true },
 ];
 
+// 미리보기 관련 변수
+const expanded = ref(false) // 토글 
+const selectedRow = ref(null) // 초기에는 null로 설정
+
 const clickCreateCategory = () => {
   const createUrl = 'http://localhost:8080/category/add';
   window.open(createUrl, 'popupWindow', 'width=1000,height=1200');
 };
+
+const onRowClick = (evt, row) => {
+  selectedRow.value = row // 선택된 행 데이터를 selectedRow에 저장
+}
 
 const onDbRowClick = (evt, row) => {
   const updateUrl = 'http://localhost:8080/categorie/' + row.id;
@@ -71,6 +122,9 @@ const getCategoryList = async () => {
   }
   console.log('response.data : ', response.data);
   rows.value = response.data;
+  if (rows.value.length > 0) {
+    selectedRow.value = rows.value[0] // 첫 번째 행 데이터를 선택
+  }
 };
 
 // 삭제 api
